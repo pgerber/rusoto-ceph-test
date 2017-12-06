ad5981ab19038d1f797ddbdeca8d583029955530
Peter Gerber <peter@arbitrary.ch>
2017-12-06 23:21:27 +0100

    add TLS support

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ extern crate rusoto_credential;
 extern crate rusoto_s3;
 extern crate tokio_core;
 
-use hyper::client::Client as HttpClient;
 use rusoto_core::region::Region;
 use rusoto_credential::StaticProvider;
 use rusoto_s3::{ListObjectsV2Request, S3, S3Client};
-//use tokio_core::reactor::Core as TokioCore;
 
 fn main() {
     let access_key = include_str!("../access_key").trim();
@@ -17,10 +15,7 @@ fn main() {
     let secret_key = include_str!("../secret_key").trim();
 
     let credentials = StaticProvider::new_minimal(access_key.to_owned(), secret_key.to_owned());
-    // let tokio_core = TokioCore::new().unwrap();        // Will be needed for hyper >= 0.11 but
-    // let tokio_handle = tokio_core.handle();            // not yet supported by Rusoto
-    // let http_client = HttpClient::new(&tokio_handle);  //
-    let http_client = HttpClient::new();
+    let http_client = rusoto_core::default_tls_client().unwrap();
     let client = S3Client::new(http_client, credentials, Region::Custom(endpoint.to_owned()));
 
     // check connection
